f7b0e18bf12f70fc4e2fcdf524f7acbd4fb8f346
Anmol Mishra <anmolmishraa56113@gmail.com>
2025-09-27 17:49:33 +0530

    basic

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("Anoml")
+const pi= 3.14;
+let name = "Anmol"
+var title = "Mishra"
+city = "Deoria"
+let accountState = null
+// alert(3+3) // we are using node.js Not browser
+console.table([pi,name , title , city,accountState])
+console.log(typeof(undefined))
