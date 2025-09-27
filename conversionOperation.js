a113ea7bb0a23b20272337338e4f470dce1e2c8c
Anmol Mishra <anmolmishraa56113@gmail.com>
2025-09-27 18:19:32 +0530

    Conversion dataType

---
+++ conversionOperation.js
@@ -0,0 +1,22 @@
+
+let score = "33abs";
+
+console.log(typeof(score))
+
+// let scoreInNumber = Number(score)
+// console.log(score)
+// console.log(typeof(scoreInNumber))
+// console.log(scoreInNumber)
+
+let isLogged = 0;
+
+let BooleanIsLogged = Boolean(isLogged)
+
+console.log(BooleanIsLogged)
+
+// 0 -> false; 1 -> true;
+// "" -> false; "AnyValue" -> true;
+
+let num = 23;
+let stringNum = String(num)
+console.log(typeof(stringNum))
