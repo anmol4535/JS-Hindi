ddee697a3e1fdb70e89bd63456bc20e2dc7c5e31
Anmol Mishra <anmolmishraa56113@gmail.com>
2025-09-28 18:03:53 +0530

    number and maths

---
+++ nums_and_math.js
@@ -0,0 +1,36 @@
+// ++++++++++++++++++ NUmber +++++++++++++++++++++++
+ const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2))
+
+
+const otherNumber = 123.4932
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 300000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// +++++++++++++++++++ Maths +++++++++++++++++++++++++++
+
+console.log(Math)
+
+const number = 100;
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(6.9))
+// console.log(Math.sqrt(100))
+// console.log(Math.pow(4,2))
+
+// console.log(Math.random())
+
+// important
+const min =  10;
+const max = 20;
+
+console.log((Math.random() * (max-min+1)) + min)
