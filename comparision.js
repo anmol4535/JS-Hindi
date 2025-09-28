603748aea0a0ff23f2284e7de52b9c1e8563a1eb
Anmol Mishra <anmolmishraa56113@gmail.com>
2025-09-28 14:44:09 +0530

    comparision

---
+++ comparision.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log(3>9)
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// in javascript the comparision and equality check works differently 
+// that is why it give the unexpected answer 
+
+//  Strick check ===
+
+console.log("2" === 2);
