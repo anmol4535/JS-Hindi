77e85a2f95e162c88f106251f1897d85abf7fa8d
Anmol Mishra <anmolmishraa56113@gmail.com>
2025-10-02 09:49:58 +0530

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+let i = 0
+while (i < 19) {
+    console.log(`value is ${i}`)
+    i +=2
+}
+
+
+// do-while loop
+ i = 10;
+do {
+    console.log(`value of i is ${i}`)
+    i++;
+} while (i <= 3);
